3f999e42f92f97628a7727e2805cd0207bc7ec3e
AlbertDEV <81445004+AlbertRSCruz@users.noreply.github.com>
2021-10-26 23:11:19 -0300

    ajustependente_apenas_em_quadro_perfeito

--- src/katas1.js
+++ src/katas1.js
@@ -1,108 +1,155 @@
+// Retornar os números de 1 a 20. (1, 2, 3,…, 19, 20)
+function contaratevinte(){
 
+  let meuRetorno = [];
+  
+              for (let counter = 1;counter <= 20;counter++) {
+                   meuRetorno.push(counter);
+              }      
+              return meuRetorno; 
+          }
+// Mostra resultado              
+    console.log(contaratevinte());
 
-for (let counter = 1;counter <= 20;counter = counter++) {
-  console.log(counter);
-}
+// Fim
 
+//.........................................................................................
 
-// //call function oneThroughTwenty
-
+// Retornar os números pares de 1 a 20. (2, 4, 6,…, 18, 20)
 function retornarPares(numero) {
+
+  let meuRetorno =[];
 
   for (let contador = 1;contador <= numero;contador++){
       if (contador % 2 === 0 ){
-          console.log(contador);
-           }           
+          meuRetorno.push(contador);
+           }      
+               
       }
+      return meuRetorno; 
   }
-  retornarPares(20)
+  // Mostra resultado  
+    console.log(retornarPares(20));
 
-//function retornarimpares(numero){
+// Fim
+
+//.........................................................................................
+
+  // Retornar os números ímpares de 1 a 20. (1, 3, 5,…, 17, 19)
   function retornarimpares(numero) {
-  for (let contador = 1;contador <= numero;contador++){
-    if (contador % 2 != 0 ){
-        console.log(contador);
-         }           
+
+    let meuRetorno =[];
+
+        for (let contador = 1;contador <= numero;contador++){
+            if (contador % 2 !== 0 ){
+                meuRetorno.push(contador);
+            }      
+                    
+        }return meuRetorno; 
     }
 
-}
+// Mostra resultado  
+    console.log(retornarimpares(20));
 
-retornarimpares(20)
+// Fim
 
+//.........................................................................................
 
-// function multiplesOfFive
-for (let counter = 5;counter <= 100;counter = counter+5) {
-  console.log(counter);
+// Retornar os múltiplos de 5 até 100. (5, 10, 15,…, 95, 100)
+function multiplosdecinco(numero){
 
-}
+  let meuRetorno = [];
 
+      for (let counter = 5;counter <= numero;counter = counter+5){
+          meuRetorno.push(counter)
+      }
+  
+      return meuRetorno;
+  }
+// Mostra resultado
+      console.log(multiplosdecinco(100));
 
-// //call function multiplesOfFive
+// Fim
 
-// function squareNumbers() {
-    
-//  /* Your code goes below
-//   Write a for or a while loop
-//   return the result */
-    
-// }
+//.........................................................................................
 
-// //call function squareNumbers
+// Retornar todos os números até 100 que forem quadrados perfeitos. (1, 4, 9, …, 81, 100)
+// Mostra resultado
+// Fim
 
-// function countingBackwards() {
-    
-let numero = 20;
-while(numero >= 2){
-  console.log(numero);
-  numero = numero - 1;
- }
-// }
+//.........................................................................................
 
-// //call function countingBackwards
+// Retornar os números contando de trás para frente de 20 até 1. (20, 19, 18, …, 2, 1)
+function retornarimpares() {
 
-// function evenNumbersBackwards() {
-    
-  for (let counter = 20;counter >= 1;counter = counter-2) {
-    if (counter % 2 === 0 ){
-        console.log(counter);
-         }           
+  let meuRetorno =[];
 
-}
+      for (let contador = 20;contador >= 1;contador = contador -1){
+          if (contador % 2 !== 0 ){
+              meuRetorno.push(contador);
+          }      
+                  
+      }return meuRetorno; 
+  }
+// Mostra resultado  
+      console.log(retornarimpares());
+// Fim
 
-// //call function evenNumbersBackwards
+//.........................................................................................
 
-// function oddNumbersBackwards() {
-    
-  for (let counter = 20;counter >= 1;counter = counter-1) {
-    if (counter % 2 !== 0 ){
-        console.log(counter);
-         }           
+// Retornar os números pares de 20 até 1. (20, 18, 16, …, 4, 2)
+function retornarPares() {
+  let meuRetorno =[];
 
-}
-// //call function oddNumbersBackwards
+  for (let contador = 20;contador >= 1;contador = contador -1){
+      if (contador % 2 === 0 ){
+          meuRetorno.push(contador);
+           }      
+               
+      }
+      return meuRetorno; 
+  }
+// Mostra resultado
+      console.log(retornarPares());
+// Fim
 
-// function multiplesOfFiveBackwards() {
-  function retornamultiplos(Maximo){
-    function variable (Minimo){
-    
-    for (let counter = Maximo;counter >= Minimo;counter = counter-5) {
-        console.log(counter);
-    
-                     }
-                 }
-                 variable(5)
-            }
-                        
-        
-    
-    retornamultiplos(100)
-// //call function multiplesOfFiveBackwards
+//.........................................................................................      
 
-// function squareNumbersBackwards() {
-    
-//    /* Your code goes below
-//   Write a for or a while loop
-//   return the result */
-// }
+// Retornar os números ímpares de 20 até 1. (19, 17, 15, …, 3, 1)
+function retornarimpares() {
+  let meuRetorno =[];
+      for (let contador = 20;contador >= 1;contador = contador -1){
+          if (contador % 2 !== 0 ){
+              meuRetorno.push(contador);
+          }      
+                  
+      }return meuRetorno; 
+  }
+// Mostra resultado  
+      console.log(retornarimpares());
+// Fim
 
-// //call function squareNumbersBackwards
+//.........................................................................................    
+
+// Retornar os múltiplos de 5 contando de trás para frente a partir de 100. (100, 95, 90, …, 10, 5)
+function multiplosdecinco(){
+
+  let meuRetorno = [];
+
+      for (let counter = 100;counter >= 5;counter = counter = counter -5){
+          meuRetorno.push(counter)
+      }
+  
+      return meuRetorno;
+  }
+// Mostra resultado  
+  console.log(multiplosdecinco());
+// Fim
+
+//.........................................................................................     
+
+// Retornar os quadrados perfeitos contando de trás para frente a partir de 100. (100, 81, 64, …, 4, 1)
+// Mostra resultado 
+// Fim
+
+//.........................................................................................    
